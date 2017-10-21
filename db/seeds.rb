347d56b7d614914f8bba8e684e596eeddbff02ec
gotayanagida <gotayanagida5@gmail.com>
2017-10-21 20:39:42 +0900

    add device

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.new(name:"田中一郎",roma_name:"ichirotanaka",password:"aaaaaa",university:"慶應義塾大学",department:"環境情報学部",grade:1,email:"a@a.com",gender:0,birthday:"1990/1/1")
+user.save!
