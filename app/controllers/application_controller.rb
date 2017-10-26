4736a8871fcd10339d8d08b6addd93d6e40f6417
gotayanagida <gotayanagida5@gmail.com>
2017-10-26 11:13:52 +0900

    sessionに会社情報を格納

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-  def set_company()
-    if company_user != nil
-      current_company = company_user
-      binding.pry
-    end
-  end
-
 end
