df32b980db9f8440990b9bc2da4341c0ee76eabb
gotayanagida <gotayanagida5@gmail.com>
2017-11-02 13:18:39 +0900

    タスクを整理

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,10 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = current_company.tasks.all.page(params[:page]).per(20).search(params[:search]).reverse_order
+    @tasks = current_company.tasks.all.page(params[:page]).per(20).search(params[:search]).reverse_order if params[:sort] == nil && params[:uesr_id] == nil
+    @tasks = current_company.tasks.order(params[:sort]).page(params[:page]).per(20) if params[:sort] != nil
+    @tasks = current_company.tasks.where(user_id: params[:user_id]).reverse_order.page(params[:page]).per(20) if params[:user_id] != nil
+
     @tasks_not_ready = @tasks.where(status_code:0)
     @tasks_ready = @tasks.where(status_code:1)
     @tasks_doing = @tasks.where(status_code:2)
